240bd4d97d9759129f26fac4148ca0815160b44c
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-08-28 11:50:49 +0200

    image build default value of MITK_BUILD_DOCKER=OFF

--- CMakeExternals/ExternalProjectList.cmake
+++ CMakeExternals/ExternalProjectList.cmake
@@ -2,5 +2,5 @@ list(APPEND MITK_USE_Boost_LIBRARIES filesystem)
 list(REMOVE_DUPLICATES MITK_USE_Boost_LIBRARIES)
 set(MITK_USE_Boost_LIBRARIES ${MITK_USE_Boost_LIBRARIES} CACHE STRING "A semi-colon separated list of required Boost libraries" FORCE)
 
-option(MITK_BUILD_DOCKER "Build docker images." ON)
 find_program(DOCKER docker)
+option(MITK_BUILD_DOCKER "Build docker images." OFF)
